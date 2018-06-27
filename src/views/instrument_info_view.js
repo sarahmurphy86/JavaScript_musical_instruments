7d906d4c2b0ae20ddb0e99430549752dd147678d
sarahmurphy86 <sarah.graham86@gmail.com>
2018-06-27 09:01:41 +0100

    homework submission

--- src/views/instrument_info_view.js
+++ src/views/instrument_info_view.js
@@ -19,4 +19,33 @@ InstrumentInfoView.prototype.render = function(instrument){
   this.detailsElement.appendChild(infoParagraph);
 };
 
+// InstrumentInfoView.prototype.render = function(instrument){
+//   // this.detailsElement.innerHTML = '';
+//
+//   const heading = this.createHeading(instrument);
+//   const details = this.createDetails(instrument);
+//   const instrumentList = this.createInstrumentList(instrument);
+//
+//   this.detailsElement.appendChild(heading);
+//   this.detailsElement.appendChild(details);
+//   this.detailsElement.appendChild(instrumentList);
+//
+//   InstrumentInfoView.prototype.createHeading = function(instrument) {
+//     const heading = document.createElement('h2');
+//     heading.textContent = instrument.name;
+//     return heading;
+//   };
+//
+//   InstrumentInfoView.prototype.createDetails = function(instrument) {
+//     const details = document.createElement('div');
+//     details.textContent = instrument.details;
+//     return details;
+//   };
+//
+//   InstrumentInfoView.prototype.createInstrumentList = function(instrument) {
+//   const instrumentList = document.createElement('ul');
+//   instrumentList.textContent = instrument.instruments;
+//   return instrumentList;
+// };
+
 module.exports = InstrumentInfoView;
